c9f9cad33257bf02ba6c4ae47908ac9da80cc07c
agostinopique <ago.pique@yahoo.it>
2022-05-02 16:39:36 +0200

    First Commit; Aggiunta testo in pagina e immagine tramite VueJS;

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+const app = new Vue({
+
+    el: '#app',
+
+    data : {
+        message: 'Hello World!',
+        image: 'https://img-prod.tgcom24.mediaset.it/images/2022/04/29/034315964-bad73288-c191-4972-b488-03597b24edfb.jpg',
+        imageAlt: 'Elon Musk'
+    }
+})
